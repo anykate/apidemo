82b2e922b7804c59616321ce7ca29882a2983696
Aniket Aryamane <aryamane.aniket@gmail.com>
2020-11-26 18:26:24 +0530

    Setting Rest Framework options

--- apidemo/settings.py
+++ apidemo/settings.py
@@ -128,3 +128,14 @@ STATIC_URL = '/static/'
 STATIC_ROOT = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
