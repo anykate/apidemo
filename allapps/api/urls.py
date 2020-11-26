6e54442f725b54c315b05870b5b7a1e87f0c258d
Aniket Aryamane <aryamane.aniket@gmail.com>
2020-11-26 18:06:44 +0530

    Added Post method

--- allapps/api/urls.py
+++ allapps/api/urls.py
@@ -6,4 +6,6 @@ app_name = 'api'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('api/v1/words/', views.WordListAPIView.as_view(),
+         name='word_list_api_view'),
 ]
--- allapps/api/views.py
+++ allapps/api/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .models import Word
+from .serializers import WordSerializer
 
 
 # Create your views here.
 def index(request):
     return render(request, 'api/index.html', {})
+
+
+class WordListAPIView(APIView):
+    def get(self, request):
+        words = Word.objects.all()
+        serializer = WordSerializer(words, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = WordSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
